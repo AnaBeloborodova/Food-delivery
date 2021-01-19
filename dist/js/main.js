1d80268dab46a939ec5b04dfe61df216e563f274
AnaBeloborodova <uroslovanasta@gmail.com>
2021-01-19 17:16:39 +0700

    :feat: add files .js

---
+++ dist/js/main.js
@@ -0,0 +1,22 @@
+$('.basic-form').validate({
+  rules: {
+    // simple rule, converted to {required:true}
+    userName: "required",
+    // compound rule
+    userPhone: {
+      required: true,
+      tel: true
+    }
+  },
+  messages: {
+    userName: {
+      required: "Имя обязательно",
+      minlength: "Имя не короче двух букв"
+    },
+    userPhone: {
+      required: "Обязательно укажите телефон"
+    }
+  },
+  $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
+
+});
